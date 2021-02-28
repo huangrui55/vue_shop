e04f97fba42c64feee4a5f8fbdb75f9a63182903
hrg <522601463@qq.com>
2021-02-28 20:09:00 +0800

    电商管理

---
+++ src/components/Welcome.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="abc">
+    <h2>welcome</h2>
+    <p>来到电商管理系统</p>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="less" scoped>
+.abc {
+  font-size: 30px;
+  text-align: center;
+  color: #319dd4;
+}
+</style>
